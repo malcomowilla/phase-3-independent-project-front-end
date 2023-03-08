af487f2e6676c004891eb63e3b1f5cb49a653488
malcomowilla <owillamalcom@gmail.com>
2023-03-08 16:42:52 +0300

    start on the project

---
+++ movie-finder/src/App.js
@@ -0,0 +1,61 @@
+ import {useEffect, useState} from 'react'
+import './App.css';
+import MovieCard from './MovieCard';
+
+function App() {
+
+  // const API_URL ="https://api.themoviedb.org/3/movie/popular?api_key=fa1192549721df01a1fb28a7788e6608"
+  // const API_SEARCH ="https://api.themoviedb.org/3/search/movie?api_key=fa1192549721df01a1fb28a7788e6608&query="
+
+
+  const [movies, setMovies]= useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    // API_URL
+  fetch()
+    .then((res) => res.json())
+    .then((data) => {
+      setMovies(data.results)
+    } )
+  },[])
+
+
+
+    console.log(movies)
+   
+    
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+// API_SEARCH+searchTerm
+      fetch()
+        .then((res) => res.json())
+        .then(data => {
+        setMovies(data.results)
+      })
+    }
+
+      console.log(searchTerm)
+  return (
+    <div className="App">
+      
+      <div className='form'>
+        <form onSubmit={handleSubmit}>
+          <input onChange={(e) => setSearchTerm(e.target.value)} />
+          <button type='submit' >Submit</button>
+        </form>
+      </div>
+
+      <div className='appContainer'>
+        {
+          movies.map(el => (
+            <MovieCard key={el.id} {...el}  />
+        ))}
+       </div> 
+       
+       </div>
+    
+  );
+}
+
+export default App;
